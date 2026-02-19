e68783657084ceef35b5ea4ef46d0965f43af05f
Bruce W. Herr II <bherr@iu.edu>
2026-02-19 16:54:28 -0500

    Add wpp schemas

--- jobs/weekly-rebuild.sh
+++ jobs/weekly-rebuild.sh
@@ -4,6 +4,10 @@ shopt -s extglob
 set -ev
 
 time jobs/pull-wpp-drafts.sh
+
+time do-processor build --clean schema/wpp/draft
+time do-processor build --clean schema/wpp-metadata/draft
 time do-processor build --clean collection/wpp/draft
+
 time do-processor finalize --skip-db
 time jobs/fix-dist-dir.sh
