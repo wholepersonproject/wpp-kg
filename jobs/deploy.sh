c3a3d12af2ec57259f5bd4d799b17e6dc7a65c1d
Bruce W. Herr II <bherr@iu.edu>
2026-03-30 12:13:15 -0400

    Fix deploy

--- jobs/deploy.sh
+++ jobs/deploy.sh
@@ -9,6 +9,6 @@ if [ "${CDN_S3_BUCKET}" != "" ]; then
   aws s3 sync --profile wpp --exclude "${DEPLOY_HOME}/.git/*" $CLEAN ${DEPLOY_HOME}/ ${CDN_S3_BUCKET}
 
   if [ "${CDN_DISTRIBUTION_ID}" != "" ]; then
-    aws cloudfront create-invalidation --distribution-id ${CDN_DISTRIBUTION_ID} --paths "/digital-objects/*"
+    aws cloudfront --profile wpp create-invalidation --distribution-id ${CDN_DISTRIBUTION_ID} --paths "/digital-objects/*"
   fi
 fi
