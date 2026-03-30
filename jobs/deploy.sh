51cffa3432a52569c998d8214c9c52505fcc0f98
Bruce W. Herr II <bherr@iu.edu>
2026-03-30 11:48:46 -0400

    Do an invalidation after deploying automatically

--- jobs/deploy.sh
+++ jobs/deploy.sh
@@ -7,4 +7,8 @@ CLEAN="--delete"
 
 if [ "${CDN_S3_BUCKET}" != "" ]; then
   aws s3 sync --profile wpp --exclude "${DEPLOY_HOME}/.git/*" $CLEAN ${DEPLOY_HOME}/ ${CDN_S3_BUCKET}
+
+  if [ "${CDN_DISTRIBUTION_ID}" != "" ]; then
+    aws cloudfront create-invalidation --distribution-id ${CDN_DISTRIBUTION_ID} --paths "/digital-objects/*"
+  fi
 fi
