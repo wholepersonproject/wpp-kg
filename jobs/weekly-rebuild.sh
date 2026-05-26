224edec8a5b1b45e030310c278534d05a7e1480b
Bruce W. Herr II <bherr@iu.edu>
2026-05-26 12:54:15 -0400

    Added logic for building every version of all wpp tables

--- jobs/weekly-rebuild.sh
+++ jobs/weekly-rebuild.sh
@@ -4,11 +4,11 @@ shopt -s extglob
 set -ev
 
 time jobs/pull-wpp-drafts.sh
-
+time jobs/build-all-wpp-versions.sh
 time do-processor build --clean schema/wpp/draft
 time do-processor build --clean schema/wpp-metadata/draft
 time do-processor build --clean collection/wpp/draft
-
 time do-processor finalize --skip-db
+time do-processor create-db --include-all-versions --journal dist/blazegraph.jnl
 time jobs/fix-dist-dir.sh
 time jobs/deploy.sh
