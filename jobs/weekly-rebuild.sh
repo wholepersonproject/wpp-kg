b441a30b3317b2154136f00c8877d935ffd828b6
Bruce W. Herr II <bherr@iu.edu>
2026-08-13 09:30:55 -0400

    Updated author information

--- jobs/weekly-rebuild.sh
+++ jobs/weekly-rebuild.sh
@@ -3,11 +3,11 @@ source constants.sh
 shopt -s extglob
 set -ev
 
-time jobs/pull-wpp-drafts.sh
-time jobs/build-all-wpp-versions.sh
-time do-processor build --clean schema/wpp/draft
-time do-processor build --clean schema/wpp-metadata/draft
-time do-processor build --clean collection/wpp/draft
+# time jobs/pull-wpp-drafts.sh
+# time jobs/build-all-wpp-versions.sh
+# time do-processor build --clean schema/wpp/draft
+# time do-processor build --clean schema/wpp-metadata/draft
+# time do-processor build --clean collection/wpp/draft
 time do-processor finalize --skip-db
 time do-processor create-db --include-all-versions --journal dist/blazegraph.jnl
 time jobs/fix-dist-dir.sh
